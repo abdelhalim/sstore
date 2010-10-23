1f76e9ae761a145fa31104fe39df74ff4bab0130
Abdelhalim Ragab <abdelhalim@blk.(none)>
2010-10-22 21:41:05 -0700

    quick and dirty test method

---
+++ test_sstore.c
@@ -0,0 +1,49 @@
+/*
+ * Abdelhalim Ragab
+ *
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+struct data_buffer {
+  int index;
+  int size;
+  char *data;
+};
+
+int sstore_dev = -1;
+
+int main() {
+
+  struct data_buffer buf;
+  int written = 0;
+  
+
+  printf("Opening sstore0 ..\n"); 
+  sstore_dev = open("/dev/sstore0", O_RDWR, S_IRWXU);
+  if (sstore_dev < 0)
+	perror("opening sstore0");
+
+  buf.index = 3;
+  buf.size = 4;
+  buf.data = (char *) malloc(5 * sizeof (char));
+  if (!buf.data)
+     printf("Error allocating memory\n");
+  
+  strncpy(buf.data, "data\0", 5);
+  perror("strncpy");
+  written = write(sstore_dev, &buf, sizeof (struct data_buffer));
+  if (written < 0)
+    perror("write");
+
+  close(sstore_dev);
+  
+
+
+}
