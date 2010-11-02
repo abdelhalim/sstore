ded4e6a0b99e8a4eb17255c41a4f2c7e9726144f
Abdelhalim Ragab <abdelhalim@blk.(none)>
2010-11-02 07:50:41 -0700

    Add some comments

--- sstore.h
+++ sstore.h
@@ -15,11 +15,13 @@
 
 #include <linux/ioctl.h>
 
+/* IOCTL operations */
 #define SSTORE_IOC_MAGIC 'v'
 
 /* Remove blob from the sstore */
 #define SSTORE_IOCREMOVE _IOW(SSTORE_IOC_MAGIC, 1, int)
 
+/* data structure used for read/write operations */
 struct data_buffer {
     int index;      /* index into the blob list */
     int size;       /* size of the data transfer */
